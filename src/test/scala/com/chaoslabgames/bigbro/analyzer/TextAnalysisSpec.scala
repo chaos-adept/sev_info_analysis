0537a823cbeb84a3b18e535b78500c193835c44f
Denis Rykovanov <drykovanov@wiley.com>
2015-08-27 14:00:14 +0300

    [+] cosmetics

--- src/test/scala/com/chaoslabgames/bigbro/analyzer/TextAnalysisSpec.scala
+++ src/test/scala/com/chaoslabgames/bigbro/analyzer/TextAnalysisSpec.scala
@@ -18,14 +18,18 @@ class TextAnalysisSpec extends FlatSpec with Matchers {
 
   it should "calculate terms count" in {
     val analyzer = new TextAnalyzer
+
     analyzer.push("i want by a Car")
+
     analyzer.termCount should be (5)
   }
 
   it should "calculate term occurrence" in {
     val analyzer = new TextAnalyzer
+
     analyzer.push("Car is bad")
     analyzer.push("Car is good")
+
     analyzer.term("Car").occurrence should be (2)
     analyzer.term("bad").occurrence should be (1)
     analyzer.term("good").occurrence should be (1)
