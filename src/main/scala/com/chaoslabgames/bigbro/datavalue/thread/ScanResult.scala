681b4fa234c97129dad236d40ab78e750d6edb62
Denis Rykovanov <drykovanov@wiley.com>
2015-08-27 19:11:44 +0300

    [+] Mock ForumThreadScanner

---
+++ src/main/scala/com/chaoslabgames/bigbro/datavalue/thread/ScanResult.scala
@@ -0,0 +1,9 @@
+package com.chaoslabgames.bigbro.datavalue.thread
+
+/**
+ * @author <a href="mailto:denis.rykovanov@gmail.com">Denis Rykovanov</a>
+ *         on 27.08.2015.
+ */
+class ScanResult(val topic:String, val totalPosts:Int) {
+
+}
