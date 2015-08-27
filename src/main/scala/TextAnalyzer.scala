f9d755d34b435168a80affa82bf7a98f1a3e0d38
Denis Rykovanov <drykovanov@wiley.com>
2015-08-27 12:47:52 +0300

    [+] basic term analyzer

---
+++ src/main/scala/TextAnalyzer.scala
@@ -0,0 +1,47 @@
+import java.util.concurrent.atomic.AtomicInteger
+
+import scala.collection.mutable
+
+/**
+ * @author <a href="mailto:denis.rykovanov@gmail.com">Denis Rykovanov</a>
+ *         on 27.08.2015.
+ */
+class TextAnalyzer {
+  var totalDocNum = 0
+  val termDict = new mutable.HashMap[String, Term]
+
+  def push(docText: String) = {
+    totalDocNum += 1
+
+    val tokens = docText.split(" ")
+
+    tokens.foreach { token =>
+      val term = termDict.getOrElseUpdate(token, new Term(token, 0, 0))
+      term.occurrence += 1
+    }
+
+    //to binary occurrence
+    tokens.toSet[String].foreach { token =>
+      termDict.get(token).get.docNum += 1
+    }
+  }
+
+  def termCount():Int = {
+    termDict.size
+  }
+
+  def term(term:String):Term = {
+    termDict.getOrElse(term, new Term(term, 0, 0))
+  }
+
+  def topTerms(num:Int):Seq[TermWeight] = {
+    //calc basic tf idf for each term
+    val termWeights = termDict.mapValues( term => Math.log(term.occurrence) * (1 - term.docNum.toDouble/totalDocNum.toDouble)).toSeq
+    val tops = termWeights.sortBy(_._2).reverse.take(num).map( it => new TermWeight(name = it._1, value = it._2) )
+    tops.toSeq
+  }
+
+}
+
+class Term(val name:String, var occurrence:Int, var docNum:Int)
+class TermWeight(val name:String, val value:Double)
---
+++ src/test/scala/TextAnalysisSpec.scala
@@ -0,0 +1,40 @@
+import org.scalatest._
+import scala.collection.Seq
+
+/**
+ * @author <a href="mailto:denis.rykovanov@gmail.com">Denis Rykovanov</a>
+ *         on 27.08.2015.
+ */
+class TextAnalysisSpec extends FlatSpec with Matchers {
+  it should " consume documents" in {
+    val analyzer = new TextAnalyzer
+    analyzer.push("i want by a Car.")
+    analyzer.push("i have the Car.")
+    analyzer.totalDocNum should be (2)
+  }
+
+  it should "calculate terms count" in {
+    val analyzer = new TextAnalyzer
+    analyzer.push("i want by a Car")
+    analyzer.termCount should be (5)
+  }
+
+  it should "calculate term occurrence" in {
+    val analyzer = new TextAnalyzer
+    analyzer.push("Car is bad")
+    analyzer.push("Car is good")
+    analyzer.term("Car").occurrence should be (2)
+    analyzer.term("bad").occurrence should be (1)
+    analyzer.term("good").occurrence should be (1)
+  }
+
+  it should "calculate top terms" in {
+    val analyzer = new TextAnalyzer
+    analyzer.push("it is red")
+    analyzer.push("it is blue")
+    analyzer.push("it is red or it is quite red")
+
+    analyzer.topTerms(2).map(_.name) should equal (Seq("red", "blue"))
+  }
+
+}
