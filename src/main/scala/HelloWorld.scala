f9d746b514b71ff39eaf8e835cd49b1f506d489d
Denis Rykovanov <drykovanov@wiley.com>
2015-08-27 10:04:24 +0300

    [+] init commit, gradle, testing

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,7 +1,3 @@
-/**
- * @author <a href="mailto:drykovanov@wiley.com">Denis Rykovanov</a>
- *         on 27.08.2015.
- */
 object HelloWorld {
   def main(args:Array[String]):Unit = {
     println("hello world")
